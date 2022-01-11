aeff6d1efc9cb122fe18d9dbd631f92c782a0218
jithendraathipatla <athipatala.jithendra@gmail.com>
2022-01-11 11:44:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'microsoftLogin';
+
+  constructor(private msalService: MsalService){
+
+  }
+
+  isLoggedIn() : boolean{
+    return this.msalService.instance.getActiveAccount() != null
+  }
+  
+  login(){
+    this.msalService.loginPopup().subscribe((response: AuthenticationResult) => {
+      this.msalService.instance.setActiveAccount(response.account)
+    });
+  }
+
+  logout(){
+    this.msalService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '8386efec-d021-4f0a-9c85-ea11809d4046',
+      redirectUri: 'http://localhost:4200'
+    }
+  })
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +21,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule,
   ],
-  providers: [],
+  providers: [{
+    provide: MSAL_INSTANCE,
+    useFactory: MSALInstanceFactory
+  },
+    MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
